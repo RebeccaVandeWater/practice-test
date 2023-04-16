58fea8a58bda518540f86fcc54d8fd7aeef12373
Rebecca VandeWater <becca.vandewater@gmail.com>
2023-04-16 14:27:44 -0600

    Complete!

--- app.js
+++ app.js
@@ -18,4 +18,62 @@ const cases = [
 
 // YOUR JS CODE
 
+let currentYear=2023
+let jsReleaseYear=1995
+
+console.log("the current age of javascript is", currentYear-jsReleaseYear)
+
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
+
+function caseDetails(){
+  
+  for(let i=0;i<cases.length;i++){
+
+  let book=cases[i]
+
+    // console.log(book.title, book.releaseYear)
+
+    if(book.releaseYear===1903){
+      console.log(book.title, book.releaseYear)
+    }
+
+  }
+
+}
+
+function everyNum(){
+
+  console.log("Starting")
+    for(i=0; i<=100; i++){
+  
+      if(i%10===0){
+        console.log(i);
+      }
+  
+      if(i===50){
+        console.log("Halfway");
+      }
+  
+    }
+  
+  console.log("Finished")
+  
+  }
+
+function oldEnough(){
+
+  let jsAge = 100
+
+  if(jsAge>=ageToVote){
+    console.log("JavaScript is old enough to vote.");
+  } else{
+    console.log("JavaScript is not old enough to vote.");
+  }
+
+}
+
+
+
+
+
 
